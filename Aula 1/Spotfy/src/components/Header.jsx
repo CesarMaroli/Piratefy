d5e659141be78baffbfc245af1d4c87e92070483
CesarMaroli <cemarcard@gmail.com>
2025-02-11 21:59:37 -0300

    gg

--- Aula 1/Spotfy/src/components/Header.jsx
+++ Aula 1/Spotfy/src/components/Header.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import logoSpotfy from '../assets/logo/logosem.png'
+import React from 'react';
+import logoSpotfy from '../assets/logo/logosem.png';
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
-  return <div className='header'>
-    <img src ={logoSpotfy} alt='Logo' />
-    <a className='header__link' href='/'>
-    <h1>Piratefy</h1>    
-    </a>
+  return (
+    <div className='header'>
+      <Link to='/'>
+      <img 
+        src={logoSpotfy} 
+        alt="Logo" 
+        style={{ width: "140px", height: "100px", objectFit: "cover" }} 
+        />
+        </Link>
+      <Link className='header__link' to='/'>
+        <h1>Piratefy</h1>
+      </Link>
     </div>
-  
-}
+  );
+};
 
-export default Header
+export default Header;
